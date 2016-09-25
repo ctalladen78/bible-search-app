d5741a0ca3bd3fed9a714d8cf0f695d4a4763201
saranggani78 <saranggani78@gmail.com>
2016-09-24 17:33:54 -0700

    updated db service

--- js/services/services.js
+++ js/services/services.js
@@ -24,8 +24,11 @@ angular.module('app.services', [])
     db.info().then(
       console.log.bind(console));
     // DONE populate db with test data
+    var version = 'akjv'; // kjv, korean, web, etc
     var psalmsUrl = 'https://getbible.net/json?text=psalms&v='+version+'&callback=JSON_CALLBACK';
-    bibleScraper.getBookUngrouped(psalmsUrl).then(function(data){
+        var ezraUrl = 'https://getbible.net/json?text=ezra&v='+version+'&callback=JSON_CALLBACK';
+
+    bibleScraper.getBookUngrouped(ezraUrl).then(function(data){
       // data is an array of objects
       // push data into pouchdb
         data.forEach(function(i){
@@ -37,6 +40,9 @@ angular.module('app.services', [])
           tempDoc.verse = i.verse;
           tempDoc.text = i.text;
           tempDoc.version = i.version;
+          // TODO: categories
+          // TODO: categories.favorites (list of verses in favorites category)
+          // TODO: add/edit/delete categories
           $q.when(db.put(tempDoc).then(function(doc){
             console.log('successfully put doc', doc._id)
           }, function(){
@@ -46,6 +52,51 @@ angular.module('app.services', [])
 
       //console.log('%%% bible psalms object: ',data)
     });
+
+    initFavorites();
+  }
+
+  function reactToChanges() {
+  db.changes({live: true, since: 'now', include_docs: true}).on('change', function (change) {
+    if (change.deleted) {
+      // change.id holds the deleted id
+      onDeleted(change.id);
+    } else { // updated/inserted
+      // change.doc holds the new doc
+      onUpdatedOrInserted(change.doc);
+    }
+    renderDocs();
+  }).on('error', console.log.bind(console));
+}
+function onDeleted(id) {
+  var index = binarySearch(docs, id);
+  var doc = docs[index];
+  if (doc && doc._id === id) {
+    docs.splice(index, 1);
+  }
+}
+
+function onUpdatedOrInserted(newDoc) {
+  var index = binarySearch(docs, newDoc._id);
+  var doc = docs[index];
+  if (doc && doc._id === newDoc._id) { // update
+    docs[index] = newDoc;
+  } else { // insert
+    docs.splice(index, 0, newDoc);
+  }
+}
+function binarySearch(arr, docId) {
+  var low = 0, high = arr.length, mid;
+  while (low < high) {
+    mid = (low + high) >>> 1; // faster version of Math.floor((low + high) / 2)
+    arr[mid]._id < docId ? low = mid + 1 : high = mid
+  }
+  return low;
+}
+  // initialize favorites category as empty list so that it is ready to
+  // have verses
+  function initFavorites(){
+    // add favorites category to db
   }
   // return a list of verses given book id, chapter id
   function getChapter(bookID, chapID){
